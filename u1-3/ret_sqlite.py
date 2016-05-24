4d067b7c66fb9d2e5534539e074427d450f7e6e5
vbaii <vincent.ibia@revinate.com>
2016-05-23 21:25:45 -0700

    changes

--- u1-3/ret_sqlite.py
+++ u1-3/ret_sqlite.py
@@ -1,6 +1,7 @@
 import sqlite3 as lite
+import pandas as pd
 con = lite.connect('getting_started.db')
-
+'''
 with con:
     cur = con.cursor()
     cur.execute('select * from cities')
@@ -9,3 +10,24 @@ with con:
     
     for row in rows:
         print row
+'''
+
+with con:
+    cur = con.cursor()
+    cur.execute('select * from cities')
+
+    rows = cur.fetchall()
+    '''
+    cur.description attribute, which is a read-only attribute that
+    contains 7 tuples:
+        name
+        type_code
+        display_size
+        internal_size
+        precision
+        scale
+        null_okay
+    '''
+    cols = [desc[0] for desc in cur.description]
+    
+    df = pd.DataFrame(rows, columns=cols)

762f3223aa5a9a5df89c05e31dbbc9543e0007da
vbaii <vincent.ibia@revinate.com>
2016-06-11 20:14:20 -0700

    base level loansData

---
+++ u2-4/logreg.py
@@ -0,0 +1,19 @@
+''' linear regression assumes a continuous response from a continuous input.
+Logistic regression aaplies to the class of variables that exists as
+discrete values, like 'yes' or 'no' overweight/underwiehgt, etc. '''
+
+# odds ratios: probability of one thing happening compared to another
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+
+loansData = pd.read_csv('../loansData.csv')
+
+# clean interest rate, fico, loan length
+loansData['Interest.Rate'] = [float(int_rate[:-1])/100 for int_rate in loansData['Interest.Rate']]
+loansData['FICO.Score'] = [int(score_range.split('-')[0]) for score_range in loansData['FICO.Range']]
+loansData['Loan.Length'] = [int(term[:-7]) for term in loansData['Loan.Length']]
+
+
